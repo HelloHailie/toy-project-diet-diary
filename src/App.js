61921031503d4a96bf50957fb650b119e9373b85
HelloHailie <alsltjsals@gmail.com>
2022-06-30 23:35:24 +0900

    add Data.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Data from "./Data";
+
+import NewDiary from "./components/NewDiary/NewDiary";
+
+function App() {
+  return (
+    <div>
+      <header></header>
+      <NewDiary />
+      <Data />
+    </div>
+  );
+}
+
+export default App;
